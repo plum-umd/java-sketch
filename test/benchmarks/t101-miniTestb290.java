3009d08c9579fb6f766dc52037f74aa8a4be7a7d
Jinseong Jeon <jsjeon@cs.umd.edu>
2015-04-26 17:48:01 -0400

    sketch-frontend/src/test/sk/seq/miniTestb290

---
+++ test/benchmarks/t101-miniTestb290.java
@@ -0,0 +1,23 @@
+class Testb290 {
+    @Harness("test")
+    public static void test (int a, int b) {
+        int x = Foo.foo(a) + Boo_Moo.moo(b) + Boo_Moo.boo(a, b);
+        assert x == (a + b + a);
+    }
+}
+
+class Foo {
+    public static int foo (int x) {
+        return x;
+    }
+}
+
+class Boo_Moo {
+    public static int moo (int y) {
+        return y;
+    }
+
+    public static int boo (int x, int y) {
+       return ?? * x + ?? * y;
+    }
+}
