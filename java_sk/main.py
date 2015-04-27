19a73e99c19f5935c03e5ec18a2a7ff7d0503863
Jinseong Jeon <jsjeon@cs.umd.edu>
2015-04-27 13:15:42 -0400

    run Sketch over the encoded sketch files

--- java_sk/main.py
+++ java_sk/main.py
@@ -11,12 +11,21 @@ import encoder
 from lib.typecheck import *
 
 import util
+import sketch
 
 pwd = os.path.dirname(__file__)
 root_dir = os.path.join(pwd, "..")
 res_dir = os.path.join(root_dir, "result")
 
-conf = {}
+conf = {
+  "encoding": True,
+  "sketch": True,
+  "randassign": False,
+  "randdegree": None,
+  "parallel": False,
+  "p_cpus": None,
+  "verbose": False
+}
 
 def configure(opt):
   conf["encoding"] = opt.encoding
@@ -31,7 +40,6 @@ def configure(opt):
 @takes(list_of(str), optional(str), optional(str))
 @returns(int)
 def main(jsk_paths, out_dir=res_dir, log_lv=logging.DEBUG):
-
   ## check custom codegen was built
   codegen_jar = os.path.join(root_dir, "codegen", "lib", "codegen.jar")
   if not os.path.isfile(codegen_jar):
@@ -44,10 +52,54 @@ def main(jsk_paths, out_dir=res_dir, log_lv=logging.DEBUG):
   ast = util.toAST(jsk_paths)
   pgr = Program(ast)
   main_cls = pgr.main_cls
+  demo_name = str(main_cls.name)
 
   ## encode the program into sketch files
-  sk_dir = os.path.join(out_dir, '_'.join(["sk", str(main_cls.name)]))
-  encoder.to_sk(pgr, sk_dir)
+  sk_dir = os.path.join(out_dir, '_'.join(["sk", demo_name]))
+  if conf["encoding"]:
+    encoder.to_sk(pgr, sk_dir)
+  else: # not encoding
+    logging.info("pass the encoding phase; rather use previous files")
+
+  opts = [] ## sketch options
+  if "verbose" in conf.keys() and conf["verbose"]: opts.extend(["-V", "10"])
+  # place to keep sketch's temporary files
+  opts.extend(["--fe-tempdir", out_dir])
+  opts.append("--fe-keep-tmp")
+  # custom codegen
+  opts.extend(["--fe-custom-codegen", codegen_jar])
+
+  ## run Sketch
+  output_path = os.path.join(out_dir, "output", "{}.txt".format(demo_name))
+  if conf["sketch"]:
+    if os.path.exists(output_path): os.remove(output_path)
+
+    if conf["randassign"] or conf["parallel"]:
+      opts.append("--slv-randassign")
+      opts.extend(["--bnd-dag-size", "16000000"]) # 16M ~> 8G memory
+
+    sketch.set_default_option(opts)
+
+    if conf["parallel"]:
+      ## Python implementation as a CEGIS (sketch-backend) wrapper
+      #_, r = sketch.be_p_run(sk_dir, output_path)
+      # Java implementation inside sketch-frontend
+      opts.append("--slv-parallel")
+      if conf["p_cpus"]:
+        opts.extend(["--slv-p-cpus", str(conf["p_cpus"])])
+      if conf["randdegree"]: # assume FIXED strategy
+        opts.extend(["--slv-randdegree", str(conf["randdegree"])])
+      else: # adaptive concretization
+        opts.extend(["--slv-strategy", "WILCOXON"])
+      _, r = sketch.run(sk_dir, output_path)
+    else:
+      _, r = sketch.run(sk_dir, output_path)
+    # if sketch fails, halt the process here
+    if not r: return r
+
+  else: # not running sketch
+    logging.info("pass sketch; rather read: {}".format(output_path))
+
 
   return 0
 
