00c8e9dfe98c9817ea770daac6d5b620bed62b48
josh reese <jreeseue@gmail.com>
2016-10-12 14:55:18 -0400

    change how translate is called.

--- java_sk/main.py
+++ java_sk/main.py
@@ -85,4 +85,4 @@ if __name__ == "__main__":
   OPT.prg = argv
 
   if OPT.tmpl: sys.exit(translate(**vars(OPT)))
-  sys.exit(translate(vars(OPT)))
+  sys.exit(translate(**vars(OPT)))
--- test/new_ast/Calls.java
+++ test/new_ast/Calls.java
@@ -1,24 +1,13 @@
-class A {
-    static int x;
-    int y;
-    static int m0() {
-	x = 0;
-	return x;
-    }
-    int m1() {
-	x = 1;
-	y = 1;
-	A.x = 1;
-	return y;
-    }
+int a(int x) {
+    return ?? - 1;
+}
+int b(int x) {
+    return ?? * 1;
 }
 
-class Calls {
-    harness static void test () {
-	int v0 = A.m0();
-	A a = new A();
-	int v1 = a.m1();
-
-	assert v1 == A.x;
-    }
+harness int test(int x) {
+    bit[2] b = 0;
+    // if (assert a(x) == 0);
+    // assert b(x) == 17;
+    return 0;
 }
