8d90fcf84a47f72b3eef154225086d09265da3e0
josh <jreeseue@gmail.com>
2016-12-20 12:15:31 -0500

    indented to new specification.

--- java_sk/main.py
+++ java_sk/main.py
@@ -15,87 +15,82 @@ root_dir = os.path.join(pwd, "..")
 res_dir = os.path.join(root_dir, "result")
 log_lvls = {'0':logging.NOTSET, '10':logging.DEBUG, '20':logging.INFO, '30':logging.WARNING,
             '40':logging.ERROR, '50':logging.CRITICAL}
+
 def translate(**kwargs):
-  ## logging configuration
-  log_lvl = log_lvls.get(kwargs.get('log_lvl', '10'))
-  logging.config.fileConfig(os.path.join(pwd, "logging.conf"))
-  logging.getLogger().setLevel(log_lvl)
-
-  prg = kwargs.get('prg', None)
-  out_dir = kwargs.get('out_dir', res_dir)
-  sk = kwargs.get('sketch', True)
-  sk_dir = ''
-  codegen_jar = os.path.join(root_dir, "codegen", "lib", "codegen.jar")
-  
-  logging.info('parsing {}'.format(prg))
-  prg_ast = parse(prg)
-  util.add_object(prg_ast)
-
-  encoder = Encoder(prg_ast)
-  encoder.main_cls()
-  demo_name = str(encoder.mcls)
-  sk_dir = os.path.join(out_dir, '_'.join(["sk", demo_name]))
-  encoder.sk_dir = sk_dir
-  logging.info('encoding to Sketch')
-  encoder.to_sk()
-
-  # Sketch options
-  opts = kwargs.get('opts', [])
-  # place to keep sketch's temporary files
-  opts.extend(["--fe-tempdir", out_dir])
-  opts.append("--fe-keep-tmp")
-
-  # custom codegen
-  opts.extend(["--fe-custom-codegen", codegen_jar])
-
-  #run Sketch
-  output_path = os.path.join(out_dir, "output", "{}.txt".format(demo_name))
-  if sk:
-    if os.path.exists(output_path): os.remove(output_path)
-    sketch.set_default_option(opts)
-
-    logging.info('sk_dir: {}, output_path: {}'.format(sk_dir, output_path))
-    _, r = sketch.run(sk_dir, output_path)
-
-    # if sketch fails, halt the process here
-    if not r: return 1
-
-  elif not prg:
-    parser.error("need to pass in some file")
-
-  return 0
-
+    ## logging configuration
+    log_lvl = log_lvls.get(kwargs.get('log_lvl', '10'))
+    logging.config.fileConfig(os.path.join(pwd, "logging.conf"))
+    logging.getLogger().setLevel(log_lvl)
+    
+    prg = kwargs.get('prg', None)
+    out_dir = kwargs.get('out_dir', res_dir)
+    sk = kwargs.get('sketch', True)
+    codegen_jar = os.path.join(root_dir, "codegen", "lib", "codegen.jar")
+    
+    logging.info('parsing {}'.format(prg))
+    prg_ast = parse(prg)
+    util.add_object(prg_ast)
+ 
+    encoder = Encoder(prg_ast, out_dir)
+    logging.info('encoding to Sketch')
+    encoder.to_sk()
+ 
+    # Sketch options
+    opts = kwargs.get('opts', [])
+    # place to keep sketch's temporary files
+    opts.extend(["--fe-tempdir", out_dir])
+    opts.append("--fe-keep-tmp")
+ 
+    # custom codegen
+    opts.extend(["--fe-custom-codegen", codegen_jar])
+ 
+    #run Sketch
+    output_path = os.path.join(out_dir, "output", "{}.txt".format(encoder.demo_name))
+    if sk:
+        if os.path.exists(output_path): os.remove(output_path)
+        sketch.set_default_option(opts)
+ 
+        logging.info('sk_dir: {}, output_path: {}'.format(encoder.sk_dir, output_path))
+        _, r = sketch.run(encoder.sk_dir, output_path)
+ 
+        # if sketch fails, halt the process here
+        if not r: return 1
+    elif not prg:
+        parser.error("need to pass in some file")
+ 
+    return 0
+       
 def main(prg):
-  return translate(prg=prg)
+    return translate(prg=prg)
   
 if __name__ == "__main__":
-  if len(sys.argv) < 1:
-    sys.exit("incorrect number of arguments")
-
-  from optparse import OptionParser
-  parser = OptionParser(usage="%prog [options]* [-t tmp_path]* (api_path)")
-
-  parser.add_option("-t", "--template",
-    action="append", dest="tmpl", default=[],
-    help="template folder")
-  parser.add_option("-l", "--log_lvl",
-    action="store", dest="log_lvl", default='10',
-    help="level of logging")
-  parser.add_option("-v", "--verbose",
-    action="store_true", dest="verbose", default=False,
-    help="print intermediate messages verbosely")
-  parser.add_option("-m", "--model",
-    action="store_true", dest="model", default=False,
-    help="use models of Java libraries")
-  parser.add_option("-o", "--out_dir",
-    dest="out_dir", default=None,
-    help="use models of Java libraries")
-  parser.add_option("--no-sketch",
-    action="store_false", dest="sketch", default=True,
-    help="proceed the whole process without running Sketch")
-
-  (OPT, argv) = parser.parse_args()
-  OPT.prg = argv
-
-  if OPT.tmpl: sys.exit(translate(**vars(OPT)))
-  sys.exit(translate(**vars(OPT)))
+    if len(sys.argv) < 1:
+      sys.exit("incorrect number of arguments")
+  
+    from optparse import OptionParser
+    parser = OptionParser(usage="%prog [options]* [-t tmp_path]* (api_path)")
+  
+    parser.add_option("-t", "--template",
+      action="append", dest="tmpl", default=[],
+      help="template folder")
+    parser.add_option("-l", "--log_lvl",
+      action="store", dest="log_lvl", default='10',
+      help="level of logging")
+    parser.add_option("-v", "--verbose",
+      action="store_true", dest="verbose", default=False,
+      help="print intermediate messages verbosely")
+    parser.add_option("-m", "--model",
+      action="store_true", dest="model", default=False,
+      help="use models of Java libraries")
+    parser.add_option("-o", "--out_dir",
+      dest="out_dir", default=None,
+      help="use models of Java libraries")
+    parser.add_option("--no-sketch",
+      action="store_false", dest="sketch", default=True,
+      help="proceed the whole process without running Sketch")
+  
+    (OPT, argv) = parser.parse_args()
+    OPT.prg = argv
+  
+    if OPT.tmpl: sys.exit(translate(**vars(OPT)))
+    sys.exit(translate(**vars(OPT)))
