7d2f06a4e1b68017b181fce3ba00f2b54a4025a0
josh <jreeseue@gmail.com>
2017-01-25 14:47:26 -0500

    oops, silly python and its silly tabbing

--- parser/ast/type/classorinterfacetype.py
+++ parser/ast/type/classorinterfacetype.py
@@ -5,18 +5,17 @@ from .type import Type
 class ClassOrInterfaceType(Type):
     def __init__(self, kwargs={}):
         if kwargs:
-        super(ClassOrInterfaceType, self).__init__(kwargs)
-        # ClassOrInterfaceType
-        self._scope = None
-        scope = kwargs.get(u'scope')
-        if scope:
-            scope.update({u'@t':u'ClassOrInterfaceType'})
-            self._scope = ClassOrInterfaceType(scope)
-  
-        self._any = kwargs.get('any')
-        # TypeArguments
-        # self._typeArguments = kwargs.get('typeArguments')
-  
+            super(ClassOrInterfaceType, self).__init__(kwargs)
+            # ClassOrInterfaceType
+            self._scope = None
+            scope = kwargs.get(u'scope')
+            if scope:
+                scope.update({u'@t':u'ClassOrInterfaceType'})
+                self._scope = ClassOrInterfaceType(scope)
+            self._any = kwargs.get('any')
+            # TypeArguments
+            # self._typeArguments = kwargs.get('typeArguments')
+
     @property
     def typee(self): return self
     @typee.setter
@@ -26,10 +25,10 @@ class ClassOrInterfaceType(Type):
     def scope(self): return self._scope
     @scope.setter
     def scope(self, v): self._scope = v
-  
+
     @property
     def anyCIT(self): return self._any
     @anyCIT.setter
     def anyCIT(self, v): self._any = v
-  
+
     def __str__(self): return self.name
