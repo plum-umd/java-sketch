5f3065495ead669e4bc66dc94b069555bbb8500d
josh <jreeseue@gmail.com>
2017-01-25 14:42:44 -0500

    nits

--- parser/ast/body/classorinterfacedeclaration.py
+++ parser/ast/body/classorinterfacedeclaration.py
@@ -12,17 +12,17 @@ from ..type.classorinterfacetype import ClassOrInterfaceType
 class ClassOrInterfaceDeclaration(TypeDeclaration):
     def __init__(self, kwargs={}):
         super(ClassOrInterfaceDeclaration, self).__init__(kwargs)
-    
+
         self._interface = kwargs.get('interface_', False)
         # List<TypeParameters>
         # self._typeParameters = kwargs.get('typeParameters', [])
-    
+
         # Can contain more than one item if this is an interface
         # List<ClassOrInterfaceType>
         self._extendsList = []
         if kwargs.get(u'extendsList'):
             self._add_supers(kwargs.get(u'extendsList', {}).get(u'@e', []), '_extendsList')
-    
+
         # List<ClassOrInterfaceType>
         self._implementsList = []
         if kwargs.get(u'implementsList'):
@@ -72,48 +72,48 @@ class ClassOrInterfaceDeclaration(TypeDeclaration):
     def interface(self): return self._interface
     @interface.setter
     def interface(self, v): self._interface = v
-      
+
     @property
     def typeParameters(self): return self._typeParameters
     @typeParameters.setter
     def typeParameters(self, v): self._typeParameters = v
-      
+
     @property
     def extendsList(self): return self._extendsList
     @extendsList.setter
     def extendsList(self, v): self._extendsList = v
-      
+
     @property
     def implementsList(self): return self._implementsList
     @implementsList.setter
     def implementsList(self, v): self._implementsList = v
-  
+
     @property
     def subClasses(self): return self._subClasses
     @subClasses.setter
     def subClasses(self, v): self._subClasses = v
-  
+
     @property
     def fullname(self):
-      return '_'.join(map(lambda c: c.name, self.enclosing_types()) + [self.name])
+        return '_'.join(map(lambda c: c.name, self.enclosing_types()) + [self.name])
     @fullname.setter
     def fullname(self, v): self._fullname = v
-  
+
     @property
     def typee(self): return ClassOrInterfaceType({u'name':self.name})
     @typee.setter
     def typee(self, v): self._typee = v
-    
+
     @property
     def name(self): return self._name
     @name.setter
     def name(self, v): self._name = v
-  
+
     def isinner(self): return type(self.parentNode) == ClassOrInterfaceDeclaration
     def enclosing_types(self):
         def up(n):
             return up(n.parentNode) + [n.parentNode] if type(n.parentNode) == ClassOrInterfaceDeclaration else []
         return [c for c in up(self) if c]
-  
+
     def __str__(self):
         return self.sanitize_ty(self.fullname) if self.isinner() else self.sanitize_ty(self.name)
--- parser/ast/type/primitivetype.py
+++ parser/ast/type/primitivetype.py
@@ -3,33 +3,32 @@
 from .type import Type
 
 class PrimitiveType(Type):
-  def __init__(self, kwargs={}):
-    super(PrimitiveType, self).__init__(kwargs)
-    typdct = kwargs.get('type')
-    self._type = typdct
-    self._name = typdct['name'].lower()
-    self._nameOfBoxedType = typdct.get(u'nameOfBoxedType', '')
+    def __init__(self, kwargs={}):
+        super(PrimitiveType, self).__init__(kwargs)
+        typdct = kwargs.get('type')
+        self._type = typdct
+        self._name = typdct['name'].lower()
+        self._nameOfBoxedType = typdct.get(u'nameOfBoxedType', '')
+      
+    @property
+    def nameOfBoxedType(self): return self.nameOfBoxedType
+    @nameOfBoxedType.setter
+    def nameOfBoxedType(self, v): self.nameOfBoxedType = v
 
-  @property
-  def nameOfBoxedType(self): return self.nameOfBoxedType
-  @nameOfBoxedType.setter
-  def nameOfBoxedType(self, v): self.nameOfBoxedType = v
+    @property
+    def typee(self): return self
+    @typee.setter
+    def typee(self, v): pass
 
-  @property
-  def typee(self): return self
-  @typee.setter
-  def typee(self, v): pass
+    @property
+    def in_set(self): return set([])
+    @in_set.setter
+    def in_set(self, v): pass
 
-  @property
-  def in_set(self): return set([])
-  @in_set.setter
-  def in_set(self, v): pass
+    @property
+    def out_set(self): return set([])
+    @out_set.setter
+    def out_set(self, v): pass
 
-  @property
-  def out_set(self): return set([])
-  @out_set.setter
-  def out_set(self, v): pass
-
-  def __str__(self):
-      return self.name
-  
+    def __str__(self):
+        return self.name
