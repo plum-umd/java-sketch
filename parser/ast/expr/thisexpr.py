cd68d4fed9f520f0beb44752d78a7a6b39dee464
josh <jreeseue@gmail.com>
2017-02-27 11:50:35 -0500

    inital draft of axioms in Sketch.

--- parser/ast/expr/thisexpr.py
+++ parser/ast/expr/thisexpr.py
@@ -3,13 +3,18 @@
 from .expression import Expression
 
 class ThisExpr(Expression):
-  def __init__(self, kwargs={}):
-    super(ThisExpr, self).__init__(kwargs)
+    def __init__(self, kwargs={}):
+        super(ThisExpr, self).__init__(kwargs)
 
-    # Expression classExpr
-    self._classExpr = kwargs.get(u'classExpr', None)
+        # Expression classExpr
+        self._classExpr = kwargs.get(u'classExpr', None)
 
-  @property
-  def classExpr(self): return self._classExpr
-  @classExpr.setter
-  def classExpr(self, v): self._classExpr = v
+    @property
+    def classExpr(self): return self._classExpr
+    @classExpr.setter
+    def classExpr(self, v): self._classExpr = v
+
+    @property
+    def name(self): return u'this'
+    @name.setter
+    def name(self, v): pass
