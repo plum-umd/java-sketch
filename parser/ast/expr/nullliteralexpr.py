88427c634a5705995c7912a8c9facd515af24f1b
josh <jreeseue@gmail.com>
2017-03-02 15:07:36 -0500

    gave nullliterexpr's a type

--- parser/ast/expr/nullliteralexpr.py
+++ parser/ast/expr/nullliteralexpr.py
@@ -2,6 +2,13 @@
 
 from .literalexpr import LiteralExpr
 
+from ..type.primitivetype import PrimitiveType
+
 class NullLiteralExpr(LiteralExpr):
-  def __init__(self, kwargs={}):
-    super(NullLiteralExpr, self).__init__(kwargs)
+    def __init__(self, kwargs={}):
+        super(NullLiteralExpr, self).__init__(kwargs)
+
+    @property
+    def typee(self): return PrimitiveType({u'type': {u'name': u'null'}})
+    @typee.setter
+    def typee(self, v): pass
