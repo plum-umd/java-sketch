98f3612b2207b564f96c9a28c8e1e31a0ff1196d
josh <jreeseue@gmail.com>
2017-03-17 12:56:10 -0400

    more axiom tests and some more gitignore stuff.

--- test/axioms/run_adt.py
+++ test/axioms/run_adt.py
@@ -1,4 +1,5 @@
-import subprocess, sys
+import subprocess
+import sys
 
 NUM_TESTS=subprocess.check_output(['find', 'input_adt', '-name', 'm[0-9].sk']).count('\n')
 NUM_TRIALS=int(sys.argv[1])
@@ -9,11 +10,11 @@ for i in range(NUM_TESTS):
     for j in range(NUM_TRIALS):
         print 'test {}, trial {}'.format(i,j)
         nm = 'input_adt/m{}.sk'.format(i)
-        t = subprocess.check_output(['sketch', '--fe-inc', 'input_adt/', nm],
-                                    stderr=subprocess.STDOUT)
+        cmd = ['sketch', '--slv-seed', '1489670589', '--fe-inc', 'input_adt/', nm]
+        print 'cmd:', ' '.join(cmd)
+        t = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         start = t.rfind('Total time = ') + len('Total time = ')
         times.append(float(t[start:t.find('\n', start)]))
-    # times.append(sum(times)/len(times))
     with open('results_adt.csv', 'a') as f:
         [f.write('{:.2f}\t'.format(n)) for n in times]
         f.write('\n')
