ad2ae39ef2bd22b04e99c48d8f42e836bcb06824
josh <jreeseue@gmail.com>
2017-03-29 11:31:04 -0400

    modified test scripts to work with single test file.

--- test/axioms/run_impl.py
+++ test/axioms/run_impl.py
@@ -1,21 +1,26 @@
-import subprocess, sys
+import subprocess
+import sys
+import re
 
-NUM_TESTS=subprocess.check_output(['find', 'input_impl', '-name', 'm[0-9].sk']).count('\n')
+with open('input_impl/test.sk') as f: test = f.read()
+NUM_TESTS=int(re.findall(r't[0-9]+', test)[-1][1:])
 NUM_TRIALS=int(sys.argv[1])
 
 with open('results_impl.csv', 'w') as f: pass
+with open('errors_impl.txt', 'w') as f: pass
+
 for i in range(NUM_TESTS):
     times = []
     for j in range(NUM_TRIALS):
-        print 'test {}, trial {}'.format(i,j)
-        nm = 'input_impl/m{}.sk'.format(i)
-        t = subprocess.check_output(['time', 'sketch', '--fe-inc', 'input_impl/', nm],
-                                    stderr=subprocess.STDOUT)
-        start = t.rfind('Total time = ') + len('Total time = ')
-        times.append(float(t[start:t.find('\n', start)]))
-    # times.append(sum(times)/len(times))
+        cmd = ['sketch', '--fe-def', 'TID={}'.format(i), '--fe-inc', 'input_impl/', 'input_impl/main.sk']
+        print 'test: {}, trial: {}, cmd: {}'.format(i, j, ' '.join(cmd))
+        try:
+            t = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            start = t.rfind('Total time = ') + len('Total time = ')
+            times.append(float(t[start:t.find('\n', start)]))
+        except:
+            with open('errors_impl.txt', 'a') as f: f.write('{}\n'.format(' '.join(cmd)))
+            break
     with open('results_impl.csv', 'a') as f:
         [f.write('{:.2f}\t'.format(n)) for n in times]
         f.write('\n')
-
-
