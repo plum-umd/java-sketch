a8a319afe7bbdf4a709251bcd9bb6d710d5d5750
Ben Mariano <benmariano13@gmail.com>
2017-03-28 23:43:08 -0400

    added loop bound hole test for Stack axioms

--- test/axioms_Stack/run_adt.py
+++ test/axioms_Stack/run_adt.py
@@ -4,19 +4,19 @@ import sys
 NUM_TESTS=9
 NUM_TRIALS=int(sys.argv[1])
 
-with open('results_Object.csv', 'w') as f: pass
+with open('results_adt.csv', 'w') as f: pass
 for i in range(NUM_TESTS):
     times = []
     flag = 'TID={}'.format(i)
     for j in range(NUM_TRIALS):
         print 'test {}, trial {}'.format(i,j)
-        cmd = ['sketch', '--fe-inc', 'input_Object/', 
-               'input_Object/main.sk', '--fe-def', flag]
+        cmd = ['sketch', '--fe-inc', 'input_adt/', 
+               'input_adt/main.sk', '--fe-def', flag]
 
         print 'cmd:', ' '.join(cmd)
         t = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         start = t.rfind('Total time = ') + len('Total time = ')
         times.append(float(t[start:t.find('\n', start)]))
-    with open('results_Object.csv', 'a') as f:
+    with open('results_adt.csv', 'a') as f:
         [f.write('{:.2f}\t'.format(n)) for n in times]
         f.write('\n')
