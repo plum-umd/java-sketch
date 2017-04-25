856f3a73559638f8c8c1d8a091dc5059b5bca3ed
josh reese <jreeseue@gmail.com>
2017-04-25 16:34:17 -0400

    script for formatting output of loops with tests.

---
+++ test/axioms/String/format.py
@@ -0,0 +1,15 @@
+import sys
+import re
+
+FILE_NAME=sys.argv[1]
+print FILE_NAME
+FILE_RENAME=FILE_NAME[:FILE_NAME.rfind('.')]+'.csv'
+print FILE_RENAME
+
+with open(FILE_NAME, 'r') as f: text = f.read()
+text = re.sub(r'\n', '', text)
+text = re.sub(r'Number of trials: 15Testing .+?Running test 8Test: 8, times: \[', '', text)
+text = re.sub('\]', '\n', text)
+text = re.sub(', ', '\t', text)
+print text
+with open(FILE_RENAME, 'w') as f: f.write(text)
