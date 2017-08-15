0722f1262bf1ee373008968eb01938b482a02b7a
josh <jreeseue@gmail.com>
2017-08-15 13:19:21 -0400

    fixed chilrenNodes in blockstmts

--- jskparser/ast/stmt/blockstmt.py
+++ jskparser/ast/stmt/blockstmt.py
@@ -3,23 +3,24 @@
 from . import _import
 from .statement import Statement
 
-
 class BlockStmt(Statement):
     def __init__(self, kwargs={}):
         super(BlockStmt, self).__init__(kwargs)
         locs = _import()
-    
+
         # List<Statement> stmts
         self._stmts = []
-    
+
         json_stmts = kwargs.get(u'stmts', {})
         if json_stmts:
             for s in json_stmts.get(u'@e', []):
                 stmt = locs[s[u'@t']](s)
-                if stmt: self._stmts.append(stmt)
-    
+                if stmt:
+                    self._stmts.append(stmt)
+                    self.add_as_parent([stmt])
+
         self.add_as_parent(self.stmts)
-    
+
     @property
     def stmts(self): return self._stmts
     @stmts.setter
