75f3c7e8db01383027f03e8aba38c6a57c8a13b2
Ben Mariano <benmariano13@gmail.com>
2017-10-29 15:17:02 -0400

    fixed name errors

--- axioms/util/TreeSet.java
+++ axioms/util/TreeSet.java
@@ -10,27 +10,27 @@ class TreeSet {
     @adt
     boolean contains(Object e);
 
-    axiom Object add(Object clear!(AxTreeSet s), Object e) {
+    axiom Object add(Object clear!(TreeSet s), Object e) {
     	return true;
     }
     
-    axiom Object add(Object AxTreeSet(), Object e) {
+    axiom Object add(Object TreeSet(), Object e) {
     	return true;
     }
 
-    axiom Object add(Object add!(AxTreeSet s, Object e1), Object e2) {
+    axiom Object add(Object add!(TreeSet s, Object e1), Object e2) {
     	return e2.equals(e1) ? false : add(s, e2);
     }
 
-    axiom Object contains(Object add!(AxTreeSet s, Object e1), Object e2) {
+    axiom Object contains(Object add!(TreeSet s, Object e1), Object e2) {
     	return e1.equals(e2) ? true : contains(s, e2);
     }
 
-    axiom Object contains(Object AxTreeSet(), Object e) {
+    axiom Object contains(Object TreeSet(), Object e) {
 	return false;
     }
 
-    axiom Object contains(Object clear!(AxTreeSet s), Object e) {
+    axiom Object contains(Object clear!(TreeSet s), Object e) {
     	return false;
     }
 
