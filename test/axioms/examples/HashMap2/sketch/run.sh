40494781584e134ea2acd79a930e550ce44cfd62
Ben Mariano <bmariano@wireless-10-104-126-75.umd.edu>
2017-11-08 15:35:43 -0500

    nits

--- test/axioms/examples/HashMap2/sketch/run.sh
+++ test/axioms/examples/HashMap2/sketch/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 loops="35"
-inline="3"
+inline="5"
 debug="-V 13"
 args="--fe-tempdir /Users/kathymariano/Research/java-sketch/test/axioms/examples/HashMap2/HashMap/../sketch/ --fe-keep-tmp --fe-output sk_BucketingTest --fe-inc /Users/kathymariano/Research/java-sketch/test/axioms/examples/HashMap2/HashMap/../sketch/sk_BucketingTest /Users/kathymariano/Research/java-sketch/test/axioms/examples/HashMap2/HashMap/../sketch/sk_BucketingTest/main.sk --bnd-unroll-amnt $loops --bnd-inline-amnt $inline $debug"
 
