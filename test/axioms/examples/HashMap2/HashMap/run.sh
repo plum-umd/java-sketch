4b5cf202f1c245de722a78639d23ecada7e9b03e
Ben Mariano <bmariano@wireless-10-104-126-75.umd.edu>
2017-11-08 15:23:32 -0500

    get hashmap2 example up and going

--- test/axioms/examples/HashMap2/HashMap/run.sh
+++ test/axioms/examples/HashMap2/HashMap/run.sh
@@ -1,3 +1,12 @@
 pwd=`pwd`
-cd ../../../../
-./jsk.sh $@ "${pwd}/*.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java"
+cd ../../../../../
+
+if [ "$1" == "1" ]
+then
+    shift
+    ./jsk.sh $@ "${pwd}/*.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/../result_noax/"
+elif [ "$1" == "2" ]
+then
+    shift
+    ./jsk.sh $@ "${pwd}/*.java ${pwd}/../libs/ --no-lib -o ${pwd}/../result_ax/"
+fi
