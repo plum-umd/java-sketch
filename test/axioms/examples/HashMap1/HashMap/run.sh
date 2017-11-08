e5732f6d6ff7f9dbb3dec740aa7e9b14246aced3
Ben Mariano <benmariano13@gmail.com>
2017-11-08 11:28:12 -0500

    nits

--- test/axioms/examples/HashMap1/HashMap/run.sh
+++ test/axioms/examples/HashMap1/HashMap/run.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 pwd=`pwd`
-cd ../../../../
+cd ../../../../../
 if [ "$1" == "1" ]
 then
     shift
-    ./jsk.sh $@ "${pwd}/HashTable.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/result"
+    ./jsk.sh $@ "${pwd}/HashTable.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/../result_noax"
 elif [ "$1" == "2" ]
 then
     shift
-    ./jsk.sh $@ "${pwd}/HashTable_loops.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/result"
+    ./jsk.sh $@ "${pwd}/HashTable_loops.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/../result_noax"
 elif [ "$1" == "3" ]
 then
     shift
-    ./jsk.sh $@ "${pwd}/HashTable_equals.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/result"
+    ./jsk.sh $@ "${pwd}/HashTable_equals.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/../result_noax"
+
+elif [ "$1" == "4" ]
+then
+    shift
+    ./jsk.sh $@ "${pwd}/HashTable.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java ${pwd}/../libs/ -o ${pwd}/../result_ax --no-lib"
+elif [ "$1" == "5" ]
+then
+    shift
+    ./jsk.sh $@ "${pwd}/HashTable_loops.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java ${pwd}/../libs/ -o ${pwd}/../result_ax --no-lib"
+elif [ "$1" == "6" ]
+then
+    shift
+    ./jsk.sh $@ "${pwd}/HashTable_equals.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java ${pwd}/../libs/ -o ${pwd}/../result_ax --no-lib"
 fi
