322267a01ecbe2ad9f5bc3f619d2432625bafa96
Ben Mariano <benmariano13@gmail.com>
2017-11-08 10:03:22 -0500

    updating hashmap1 example

--- test/axioms/examples/HashMap1/run.sh
+++ test/axioms/examples/HashMap1/run.sh
@@ -5,13 +5,13 @@ cd ../../../../
 if [ "$1" == "1" ]
 then
     shift
-    ./jsk.sh $@ "${pwd}/HashTable.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java"
+    ./jsk.sh $@ "${pwd}/HashTable.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/result"
 elif [ "$1" == "2" ]
 then
     shift
-    ./jsk.sh $@ "${pwd}/HashTable_loops.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java"
+    ./jsk.sh $@ "${pwd}/HashTable_loops.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/result"
 elif [ "$1" == "3" ]
 then
     shift
-    ./jsk.sh $@ "${pwd}/HashTable_equals.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java"
+    ./jsk.sh $@ "${pwd}/HashTable_equals.java ${pwd}/HashTableTest.java ${pwd}/HashTableNode.java model/org/junit/Assert.java model/lang/Integer.java model/lang/Number.java model/util/ArrayList.java -o ${pwd}/result"
 fi
