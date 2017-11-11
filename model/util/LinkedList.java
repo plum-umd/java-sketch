c9de2e653133ce35d0aa289a2949ea1c2a2ddf1c
Ben Mariano <benmariano13@gmail.com>
2017-11-11 16:50:54 -0500

    added in empty implementations for List functions

--- model/util/LinkedList.java
+++ model/util/LinkedList.java
@@ -1,12 +1,22 @@
 package java.util;
 
-public class LinkedList implements List {
+public class LinkedList<E> implements List<E> {
     public LinkedList() { };
     
     /* These are translated to uninterpreted functions in Sketch which doesn't work */
-    // public boolean add(Object e);
-    // public boolean isEmpty();
-    // public Object get(int index);
-    // public void remove(Object e);
-    // public boolean remove(int index);
+    public boolean add(Object e) {
+    	return false;
+    }
+    public boolean isEmpty() {
+    	return false;
+    }
+    public E get(int index) {
+    	return null;
+    }
+    public void remove(Object e) {
+
+    }
+    public boolean remove(int index) {
+    	return false;
+    }
 }
