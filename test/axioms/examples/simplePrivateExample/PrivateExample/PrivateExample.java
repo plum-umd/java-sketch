4e8ab6cfa2bc9d6ff6ada7d10885f1ecb33a69e0
Ben Mariano <bmariano@wireless-10-104-116-145.umd.edu>
2017-11-13 12:25:12 -0500

    simple example compiles

--- test/axioms/examples/simplePrivateExample/PrivateExample/PrivateExample.java
+++ test/axioms/examples/simplePrivateExample/PrivateExample/PrivateExample.java
@@ -7,42 +7,47 @@
 public class PrivateExample {
 
     harness void test_privateExample() {
-	// String plainText = "Secret Message!";
+	String plainText = "Secret Message!";
 
-	// byte[] newPlainText = privateExample(plainText);
+	byte[] newPlainText = privateExample(plainText);
 
-	// assertEquals(plainText, newPlainText);
+	String plainText2 = new String(newPlainText);
+	
+	assert plainText.equals(plainText2);
     }
     
-    // public static byte[] privateExample(String plainText) {
-    //     // // Check args and get plaintext
-    //     // if (args.length !=1) {
-    //     //     // System.err.println("Usage: java PrivateExample text");
-    //     //     // System.exit(1);
-    // 	//     assert false;
-    //     // }
+    public static byte[] privateExample(String plainTextString) {
+        // // Check args and get plaintext
+        // if (args.length !=1) {
+        //     // System.err.println("Usage: java PrivateExample text");
+        //     // System.exit(1);
+    	//     assert false;
+        // }
 
-    //     // // Get a DES private key
-    //     // KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+	byte[] plainText = plainTextString.getBytes();
+	
+        // Get a DES private key
+        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
         
-    //     // // If you do not initialize the KeyGenerator, each provider supply a default initialization.
-    //     // keyGen.init(56);
-    //     // Key key = keyGen.generateKey();
+        // If you do not initialize the KeyGenerator, each provider supply a default initialization.
+        keyGen.init(56);
+        Key key = keyGen.generateKey();
 
-    //     // // Creates the DES Cipher object (specifying the algorithm, mode, and padding).
-    //     // Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        // Creates the DES Cipher object (specifying the algorithm, mode, and padding).
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
 
-    //     // // Initializes the Cipher object.
-    //     // cipher.init(Cipher.ENCRYPT_MODE, key);
-    //     // // Encrypt the plaintext using the public key
-    //     // byte[] cipherText = cipher.doFinal(plainText);
+        // Initializes the Cipher object.
+        // cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(1, key);
+        // Encrypt the plaintext using the public key
+        byte[] cipherText = cipher.doFinal(plainText);
 
-    //     // // Initializes the Cipher object.
-    //     // cipher.init(Cipher.DECRYPT_MODE, key);
-    //     // // Decrypt the ciphertext using the same key
-    //     // byte[] newPlainText = cipher.doFinal(cipherText);
+        // Initializes the Cipher object.
+        // cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(2, key);
+        // Decrypt the ciphertext using the same key
+        byte[] newPlainText = cipher.doFinal(cipherText);
 
-    // 	// return newPlainText
-    // 	return new byte[3];
-    // }
+    	return newPlainText;
+    }
 }
