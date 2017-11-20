7490baf219dfe67bacf7879286aad188b309de5d
siyuanpurdue <33631195+siyuanpurdue@users.noreply.github.com>
2017-11-20 17:21:08 -0500

    Fix some errors..
    Fix some errors..

--- test/axioms/examples/PQueue/PQueue.java
+++ test/axioms/examples/PQueue/PQueue.java
@@ -9,30 +9,30 @@ import java.util.*;
 //class PQueue <T extends Comparable<T>> {
 class PQueue {
 
-  // The number of elements currently inside the heap
+ // The number of elements currently inside the heap
   private int heapSize = 0;
 
   // The internal capacity of the heap
   private int heapCapacity = 0;
 
-  
   // A dynamic list to track the elements inside the heap
-    private List <Integer> heap = null;
-  
+  private List <Integer> heap = null;
+
   // This map keeps track of the possible indices a particular 
   // node value is found in the heap. Having this mapping lets
   // us have O(log(n)) removals and O(1) element containment check
   // at the cost of some additional space and minor overhead
-   private Map <Integer, TreeSet<Integer>> map = new HashMap<>();
+  private Map <Integer, TreeSet<Integer>> map = new HashMap<>();
 
   // Construct and initially empty priority queue
   public PQueue() {
     this(1);
   }
-  
+
   // Construct a priority queue with an initial capacity
   public PQueue(int sz) {
     heap = new ArrayList<>(sz);
+    // instance variables not working??? Gotta be careful
     map = new HashMap<>();
     heapSize = 0;
     heapCapacity = 0;
@@ -49,63 +49,48 @@ class PQueue {
     return heapSize == 0;
   }
 
-  // Construct a priority queue using heapify in O(n) time, a great explanation can be found at:
-  // http://www.cs.umd.edu/~meesh/351/mount/lectures/lect14-heapsort-analysis-part.pdf
- // public PQueue (int[] elems) {
-
-	// unnecessary constructor
-  // Priority queue construction, O(nlog(n))
- // public PQueue (Collection <Integer> elems) {
-//    this(elems.size());
-//    for( int elem : elems) add(elem);
-//  }
-//------------------------------------------------------------------------------------------
-// The above working now
-  
+ // omit two constructors
 
 
- 
-
-// Add a node value and its index to the map
+ // Add a node value and its index to the map
   private void mapAdd(Integer value, int index) {
     
     Integer indexTmp = new Integer(index);
     TreeSet <Integer> set = map.get(value);
-     
-	
+
     // New value being inserted in map
     if (set == null) {
 
-     set = new TreeSet<>();
-      
+      set = new TreeSet<>();
       set.add(indexTmp);
       map.put(value, set);
 
     // Value already exists in map
     } else set.add(indexTmp);
 
-	
-   
   }
   
 
 
-  // Removes the index at a given value, O(log(n))
+   // Removes the index at a given value, O(log(n))
   private void mapRemove(Integer value, int index) {
-   Integer indexTmp = new Integer(index);
-   TreeSet <Integer> set = map.get(value);
-   set.remove(indexTmp); // TreeSets take O(log(n)) removal time
-   if (set.size() == 0) map.remove(value);   
+    Integer indexTmp = new Integer(index);
+    TreeSet <Integer> set = map.get(value);
+    set.remove(indexTmp); // TreeSets take O(log(n)) removal time
+    if (set.size() == 0) map.remove(value);
   }
 
- // Extract an index position for the given value
+  // Extract an index position for the given value
   // NOTE: If a value exists multiple times in the heap the highest 
   // index is returned (this has arbitrarily been chosen)
   private Integer mapGet(Integer value) {
     TreeSet <Integer> set = map.get(value);
-    if (set != null) return set.last();   //need to implement last!!!!!!!!!
+    if (set != null) return set.last();
     return null;
   }
+
+
+  // Removes a particular element in the heap, O(log(n))
 
 
 
@@ -115,25 +100,25 @@ class PQueue {
     if (element == null) return false;
     
     // Linear removal via search, O(n)
-     for (int i = 0; i < heapSize; i++) {
-       if (element.equals(heap.get(i))) {
-        // removeAt(i);
-         return true;
-       }
-     }
+    // for (int i = 0; i < heapSize; i++) {
+    //   if (element.equals(heap.get(i))) {
+    //     removeAt(i);
+    //     return true;
+    //   }
+    // }
 
     // Logarithmic removal with map, O(log(n))
     Integer index = mapGet(element);
-    //if (index != null) removeAt(index);
+    int indexTmp = (int) index;
+    if (index != null) 
+       removeAt(indexTmp);
     return index != null;
 
   }
 
-
-
 // Tests if the value of node i <= node j
   // This method assumes i & j are valid indices, O(1)
-  private boolean less(int i, int j) {
+   private boolean less(int i, int j) {
 
     Integer node1 = heap.get(i);
     Integer node2 = heap.get(j);
@@ -142,8 +127,7 @@ class PQueue {
   }
 
 // intop down node sink, O(log(n))
-  private void sink(int k) {
-
+   private void sink(int k) {
 
     while ( true ) {
       
@@ -158,10 +142,10 @@ class PQueue {
 
       // Stop if we're outside the bounds of the tree
       // or stop early if we cannot sink k anymore
-      if ( left >= heapSize || less(k, smallest)) return;  // Caused by this line But the terminal says something wrong with Arraylist
+      if ( left >= heapSize || less(k, smallest)) return;
       
       // Move down the tree following the smallest node
-     // swap(smallest, k);
+      swap(smallest, k);
       k = smallest;
 
     }
@@ -190,12 +174,12 @@ class PQueue {
 
 
  // Swap two nodes. Assumes i & j are valid, O(1)
-  private void swap(int i, int j) {
+ private void swap(int i, int j) {
     
     Integer i_elem = heap.get(i);
     Integer j_elem = heap.get(j);
 
-    heap.set(i, j_elem); // Something wrong with set?? But ArrayList
+    heap.set(i, j_elem);
     heap.set(j, i_elem);
 
     mapSwap(i_elem, j_elem, i, j);
@@ -203,7 +187,7 @@ class PQueue {
   }
 
   // Perform bottom up node swim, O(log(n))
-  private void swim(int k) {
+   private void swim(int k) {
     
     // Grab the index of the next parent node WRT to k
     int parent = (k-1) / 2;
@@ -264,7 +248,6 @@ class PQueue {
 
 
 
-
   // Clears everything inside the heap, O(n)
   public void clear() {
     for (int i = 0; i < heapCapacity; i++)
@@ -272,12 +255,12 @@ class PQueue {
     heapSize = 0;
     map.clear();
   }
-
     
 
  
 
-  // Returns the value of the element with the lowest
+ // Returns the value of the element with the lowest
+  // priority in this priority queue. If the priority 
   // queue is empty null is returned.
   public Integer peek() {
     if (isEmpty()) return null;
@@ -291,18 +274,17 @@ class PQueue {
 
 
 
-  // intest if an element is in heap, O(1)
-  public boolean contains(Integer elem) {
+ public boolean contains(Integer elem) {
 
     // Map lookup to check containment, O(1)
     if (elem == null) return false;
     return map.containsKey(elem);
 
     // Linear scan to check containment, O(n)
-     for(int i = 0; i < heapSize; i++)
-       if (heap.get(i).equals(elem))
-         return true;
-     return false;
+    // for(int i = 0; i < heapSize; i++)
+    //   if (heap.get(i).equals(elem))
+    //     return true;
+    // return false;
 
   }
 
@@ -311,7 +293,7 @@ class PQueue {
   // element must not be null, O(log(n))
   public void add(Integer elem) {
     
-   // if (elem == null) throw new IllegalArgumentException();
+    if (elem == null) return; //throw new IllegalArgumentException();
 
     if (heapSize < heapCapacity) {
       heap.set(heapSize, elem);
@@ -332,8 +314,25 @@ class PQueue {
   // This method is just for testing purposes to make
   // sure the heap invariant is still being maintained
   // Called this method with k=0 to start at the root
- // public boolean isMinHeap(int k)
-  
+ 
+    public boolean isMinHeap(int k) {
+
+    // If we are outside the bounds of the heap return true 
+    if (k >= heapSize) return true;
+
+    int left  = 2 * k + 1;
+    int right = 2 * k + 2;
+
+    // Make sure that the current node k is less than
+    // both of its children left, and right if they exist
+    // return false otherwise to indicate an invalid heap
+    if (left  < heapSize  && !less(k, left))  return false;
+    if (right < heapSize  && !less(k, right)) return false;
+
+    // Recurse on both children to make sure they're also valid heaps
+    return isMinHeap(left) && isMinHeap(right);
+
+  }  
  
 
 
@@ -352,24 +351,21 @@ class PQueue {
 
 
     PQueue q = new PQueue();
-
     Integer[] nums = { new Integer(3),new Integer(2), new Integer(5), new Integer(6), new Integer(7), new Integer(9), new Integer(4), new Integer(8), new Integer(1) };
-
-     // Try manually creating heap
-     
-   
-     //for (int n : nums) q.add(n);
+    
+    // Try manually creating heap
+	     
+	   
+    //for (int n : nums) q.add(n);
     for (int i = 0; i <= 8; i++ )
     {
-	 q.add(nums[i]);
+	q.add(nums[i]);
     }
 
     for (int i = 1; i <= 9; i++)
     {
-        assert ( new Integer(i) ==  q.poll() );
+	assert (i == (int)q.poll());
     }
-
-  //   assert q.isEmpty();
 
   //  assertEquals(q.poll(), null);
 
