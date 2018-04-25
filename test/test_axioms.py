f28866a2a0b1054ced1e8064fd480cdd4771d99b
Ben Mariano <bmariano@Kathys-Mac-mini.local>
2018-04-25 15:22:23 -0400

    update tester script

--- test/test_axioms.py
+++ test/test_axioms.py
@@ -11,21 +11,35 @@ tests = os.path.join(pwd, "axioms/benchmarks")
 class TestJava(TestCommon):
     def __test(self, fs, inline, unroll):
         _fs = map(lambda f: os.path.join(tests, f), fs)
-        ret = java_sk.main.translate(prg=_fs, log_lvl='30', lib=False, opts=["--bnd-inline-amnt", str(inline), "--bnd-unroll-amnt", str(unroll)])
+        ret = java_sk.main.translate(prg=_fs, log_lvl='30', lib=False, opts=["--bnd-inline-amnt", str(inline), "--bnd-unroll-amnt", str(unroll), "--slv-timeout 10"])
         self.assertEqual(ret, 0)
 
-    def test_SuffixArrayModel(self):
-        files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "model/"]        
-        files = map(lambda s: "SuffixArray/" + s, files)
-        inline = 2
-        unroll = 8
-        self.__test(files, inline, unroll)
+    # def test_SuffixArrayModel(self):
+    #     files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "model/"]        
+    #     files = map(lambda s: "SuffixArray/" + s, files)
+    #     inline = 2
+    #     unroll = 8
+    #     self.__test(files, inline, unroll)
 
-    def test_SuffixArrayRewrite(self):
-        files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "rewrite/"]        
-        files = map(lambda s: "SuffixArray/" + s, files)
-        inline = 2
-        unroll = 8
+    # def test_SuffixArrayRewrite(self):
+    #     files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "rewrite/"]        
+    #     files = map(lambda s: "SuffixArray/" + s, files)
+    #     inline = 2
+    #     unroll = 8
+    #     self.__test(files, inline, unroll)
+        
+    # def test_HashMap1Model(self):
+    #     files = ["HashTable_loops.java", "HashTableNode.java", "HashTableTest.java", "model/"]
+    #     files = map(lambda s: "HashMap1/" + s, files)
+    #     inline = 2
+    #     unroll = 13
+    #     self.__test(files, inline, unroll)
+        
+    def test_HashMap1Rewrite(self):
+        files = ["HashTable_loops.java", "HashTableNode.java", "HashTableTest.java", "rewrite/"]
+        files = map(lambda s: "HashMap1/" + s, files)
+        inline = 3
+        unroll = 2
         self.__test(files, inline, unroll)
         
 
