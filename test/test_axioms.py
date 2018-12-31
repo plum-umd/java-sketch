f663bff612fdb2e37f07d73a22413e0d4f966224
Ben Mariano <bmariano@Kathys-Mac-mini.fios-router.home>
2018-12-31 10:23:35 -0500

    tester for kafka

--- test/test_axioms.py
+++ test/test_axioms.py
@@ -21,6 +21,70 @@ class TestJava(TestCommon):
 ## MOCK TESTS
 #################################################################
 
+    def test_KafkaRewrite(self):
+        # files = ["JCECipher_syn_rewrite.java", "OpenSSLCipher_syn_rewrite.java", "CipherFactory.java", "ICipher_rewrite.java", "Tester_rewrite.java", "rewrite/", "shared/"]
+        files = ["JCECipher_syn.java", "OpenSSLCipher_syn.java", "CipherFactory.java", "ICipher_model.java", "Tester_model.java", "model/", "shared/"]
+        files = map(lambda s: "Kafka_biggest/" + s, files)
+        inline = 2
+        unroll = 35
+        # inline = 1
+        # unroll = 15
+        return self.__test(files, inline, unroll)        
+
+    # def test_SimpleArrayListZ3(self):
+    #     files = ["SimpleExample.java", "ArrayList.java", "Object.java"]
+    #     files = map(lambda s: "SimpleArrayListExampleZ3/" + s, files)
+    #     inline = 2
+    #     unroll = 8
+    #     self.__test(files, inline, unroll)
+        
+    # def test_RomList_bigger(self):
+    #     files = ["RomlistParser_syn_model.java", "RomlistGame.java", "Tester.java", "model/", "shared/"]
+    #     # files = ["RomlistParser_rewrite.java", "RomlistGame.java", "Tester.java", "rewrite/", "shared/"]
+    #     files = map(lambda s: "RomList_bigger/" + s, files)
+    #     inline = 2
+    #     unroll = 26
+    #     # unroll = 19
+    #     self.__test(files, inline, unroll, True)
+    #     # self.__test(files, inline, unroll)
+
+    # def test_ComparatorRewrite(self):
+    #     files = ["CommunicationWithFiles_syn_rewrite.java", "Comparator_rewrite.java", "Tester.java", "rewrite/", "shared/"] 
+    #     files = map(lambda s: "Comparator_bigger/" + s, files)
+    #     inline = 2
+    #     unroll = 10
+    #     self.__test(files, inline, unroll, True)            
+
+    # def test_PasswordManager_bigger(self):
+    #     files = ["Cryptographer_syn_model.java", "PasswordManager_syn.java", "PasswordMap.java", "PasswordManagerTest.java", "model/", "shared/"]
+    #     # files = ["Cryptographer_rewrite.java", "PasswordManager_syn.java", "PasswordMap.java", "PasswordManagerTest.java", "rewrite/", "shared/"]
+    #     files = map(lambda s: "PasswordManager_bigger/" + s, files)
+    #     inline = 2
+    #     unroll = 16
+    #     self.__test(files, inline, unroll)
+        
+    # def test_EasyCSVRewrite_bigger(self):
+    #     files = ["CsvDocument_syn.java", "CodeAssertion.java", "CsvColumn.java", "CsvColumnTest.java", "CsvConfiguration.java", "CsvDocumentTest.java", "CsvRow.java", "CsvRowTest.java", "Tester.java", "rewrite/", "shared/"]
+    #     files = map(lambda s: "EasyCSV_bigger/" + s, files)
+    #     inline = 3
+    #     unroll = 5
+    #     # self.__test(files, inline, unroll, True)
+    #     self.__test(files, inline, unroll)
+
+    # def test_CipherFactoryModel_bigger(self):
+    #     files = ["CryptoManager_syn_model.java", "CipherFactoryTester.java", "ConfigurableCipherFactory.java", "DefaultCipherFactory_model.java", "ICipherFactory.java", "ICryptoManager.java", "model/", "shared/"]
+    #     files = map(lambda s: "CipherFactory_bigger/" + s, files)
+    #     inline = 2
+    #     unroll = 9
+    #     self.__test(files, inline, unroll)
+
+    # def test_SuffixArrayModel_bigger(self):
+    #     files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "model/"]        
+    #     files = map(lambda s: "SuffixArray_bigger/" + s, files)
+    #     inline = 3
+    #     unroll = 8
+    #     self.__test(files, inline, unroll)
+
     # def test_SuffixArrayModel(self):
     #     files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "model/"]        
     #     files = map(lambda s: "SuffixArray/" + s, files)
@@ -91,7 +155,7 @@ class TestJava(TestCommon):
     # def test_SuffixArrayRewrite(self):
     #     files = ["SuffixArray_loops.java", "SuffixArrayTest.java", "rewrite/"]        
     #     files = map(lambda s: "SuffixArray/" + s, files)
-    #     inline = 2
+    #     inline = 3
     #     unroll = 8
     #     self.__test(files, inline, unroll)        
 
@@ -175,12 +239,12 @@ class TestJava(TestCommon):
     #     self.__test(files, inline, unroll, True)
     #     # self.__test(files, inline, unroll)
 
-    def test_ComparatorRewrite(self):
-        files = ["CommunicationWithFiles_syn_rewrite.java", "Comparator_rewrite.java", "Tester.java", "rewrite/", "shared/"] 
-        files = map(lambda s: "Comparator/" + s, files)
-        inline = 2
-        unroll = 10
-        self.__test(files, inline, unroll, True)            
+    # def test_ComparatorRewrite(self):
+    #     files = ["CommunicationWithFiles_syn_rewrite.java", "Comparator_rewrite.java", "Tester.java", "rewrite/", "shared/"] 
+    #     files = map(lambda s: "Comparator/" + s, files)
+    #     inline = 2
+    #     unroll = 10
+    #     self.__test(files, inline, unroll, True)            
 
 
 if __name__ == '__main__':
