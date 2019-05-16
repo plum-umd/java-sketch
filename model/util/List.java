9396de709a2bb3ccaec3524f4f9695112b6fa293
natebragg <nate@cs.tufts.edu>
2019-05-16 17:46:21 -0400

    Type correct API for List.remove

--- model/util/List.java
+++ model/util/List.java
@@ -4,8 +4,8 @@ public interface List <E> {
   public <E> boolean add(E e);
   public boolean isEmpty();
   public E get(int index);
-  public void remove(Object e);
-  public boolean remove(int index);
+  public E remove(int index);
+  public boolean remove(Object e);
   public <E> E set (int index, E element);
   public int size();
 }
