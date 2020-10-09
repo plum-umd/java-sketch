f9f0bd05f2059432520af45f33d95534af39c038
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-10-09 14:37:54 -0400

    Doc String for java code generator and basic data

--- codegen/src/JavaCode.java
+++ codegen/src/JavaCode.java
@@ -1,4 +1,6 @@
+import java.awt.List;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import sketch.compiler.ast.core.FEReplacer;
 //import sketch.compiler.ast.core.Package;
@@ -7,30 +9,52 @@ import sketch.compiler.ast.core.exprs.*;
 import sketch.compiler.ast.core.stmts.*;
 import sketch.util.annot.CodeGenerator;
 
+/**
+ * CodeGenerator to generate snippets of Java code to be embedded back
+ * into the input JSketch to produce runnable synthesized Java code.
+ * 
+ * As java-sketch adopts generator and choice syntaxes from Sketch. It is
+ * hard to rebuild runnable Java code from Sketch output merely from hole
+ * values, since holes are ambiguous when these syntaxes are used and internal
+ * conversion of Sketch data structures would be needed to resolve these
+ * results. Thus it is more preferable to generate Java code in small snippets
+ * (ideally per function/method) and embed snippets back to java sketch source
+ * to produce whole code.
+ * 
+ * The output of this CodeGenerator is expected to be used with an external
+ * post-processor which could embed the generated function/method snippets back.
+ */
+
 @CodeGenerator
 public class JavaCode extends FEReplacer {
 
   final PrintWriter out;
 
+  HashMap<String, List<String>> func_impl;
+
   public JavaCode() {
     out = new PrintWriter(System.out);
+    func_impl = new HashMap<String, List<String>>();
   }
 
-/*
   private String cur_pkg;
 
   @Override
   public Object visitPackage(Package spec) {
     cur_pkg = spec.getName();
-    return super.visitPackage(spec);
+    Object rtn_obj = super.visitPackage(spec);
+
+    return rtn_obj;
   }
-*/
 
   private String cur_func;
+  private List<String> cur_func_stmt_list;
 
   @Override
   public Object visitFunction(Function func) {
     cur_func = func.getName();
+    cur_func_stmt_list = new ArrayList<String>();
+    func_impl.put(cur_pkg + "::" + cur_func, cur_func_stmt_list);
     return super.visitFunction(func);
   }
 
