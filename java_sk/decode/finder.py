7d569abde2f19a47c5e32425417063e9c0dea1f0
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2021-09-22 21:58:25 -0400

    Code cleanup and check-in

--- java_sk/decode/finder.py
+++ java_sk/decode/finder.py
@@ -1,3 +1,4 @@
+from ast.body.variabledeclarator import VariableDeclarator
 import logging
 
 from ast.visit import visit as v
@@ -5,18 +6,16 @@ from ast.visit import visit as v
 from ast.node import Node
 from ast.expr.generatorexpr import GeneratorExpr
 from ast.body.classorinterfacedeclaration import ClassOrInterfaceDeclaration
-from ast.body.methoddeclaration import MethodDeclaration
 
 
 """
-Finding hole declarations
+Finding hole declarations in variable declarators
 """
 class HFinder(object):
 
     def __init__(self):
         self._holes = []
         self._cur_class = None
-        self._cur_method = None 
     @property
     def holes(self):
         return self._holes
@@ -33,19 +32,16 @@ class HFinder(object):
 
     @v.when(ClassOrInterfaceDeclaration)
     def visit(self, node):
-        self._cur_class = node.name
+        self._cur_class = node
         for c in node.childrenNodes: c.accept(self)
 
-    @v.when(MethodDeclaration)
+    @v.when(VariableDeclarator)
     def visit(self, node):
-        self._cur_method = node.name
-        for c in node.childrenNodes: c.accept(self)
+        if isinstance(node.init, GeneratorExpr) and node.init.isHole:
+            logging.debug("named hole found: {}.{}".format(self._cur_class.fullname, node.name))
+            node.class_name = self._cur_class.name
+            self._holes.append(node) 
 
-    @v.when(GeneratorExpr)
-    def visit(self, node):
-        if node.isHole:
-            logging.debug("hole@: {}.{}".format(self._cur_class, self._cur_method))
-            self._holes.append(node)
 
 
 """
@@ -56,7 +52,6 @@ class EGFinder(object):
     def __init__(self):
         self._egens = []
         self._cur_class = None
-        self._cur_method = None
 
     @property
     def egens(self):
@@ -74,16 +69,13 @@ class EGFinder(object):
 
     @v.when(ClassOrInterfaceDeclaration)
     def visit(self, node):
-        self._cur_class = node.name
+        self._cur_class = node
         for c in node.childrenNodes: c.accept(self)
 
-    @v.when(MethodDeclaration)
+    @v.when(VariableDeclarator)
     def visit(self, node):
-        self._cur_method = node.name
-        for c in node.childrenNodes: c.accept(self)
+        if isinstance(node.init, GeneratorExpr) and not node.init.isHole:
+            logging.debug("regex generator found: {}.{}".format(self._cur_class.fullname, node.name))
+            node.class_name = self._cur_class.name
+            self._egens.append(node) 
 
-    @v.when(GeneratorExpr)
-    def visit(self, node):
-        if node.isHole:
-            logging.debug("generator@: {}.{}".format(self._cur_class, self._cur_method))
-            self._egens.append(node)
